1d1913ea76e7fe2b79d82ae62a936bdc65b326bf
kothet <kothetnaingtun.ict@gmail.com>
2024-12-08 10:22:46 +0630

    check error

---
+++ backend/static/myjs/custom.js
@@ -0,0 +1,181 @@
+console.log('custome js working ....');
+
+//Start Category View 
+
+  $('#categoryview-1').DataTable(
+  {
+    autoWidth: true,
+    "lengthMenu": [
+      [16, 32, 64, -1],
+      [16, 32, 64, "All"]
+    ]
+  });
+
+  $('#saleview-2').DataTable(
+    {
+      autoWidth: true,
+      "lengthMenu": [
+        [16, 32, 64, -1],
+        [16, 32, 64, "All"]
+      ],
+      "ordering": false,
+    }
+    // { "targets": [0], "searchable": false, "orderable": false, "visible": true }
+  );
+ 
+
+new DataTable('#saleerporttbl', {
+  layout: {
+      topStart: {
+          buttons: ['copy', 'excel', 'pdf', 'colvis']
+      }
+  }
+});
+
+
+
+  $("#categorylist").on('click', '.removebtn', function() {
+    var currenttr = $(this).closest(".catlisttr");
+    var cid = currenttr.find(".cid").html();
+    // console.log(cid)
+    $.ajax({
+      url: `/category/delete/${cid}/`,
+      method: "GET",
+      data:{cid:cid},
+      success: function(data){
+        alert('Category Delete from List')
+          window.setTimeout(function(){ } ,2000);
+                          location.reload();
+  
+          // window.location.href = '/admin/';
+      },
+      error:function(){
+          alert('having some error contact to developer');
+      },
+                      
+    });//end ajax
+  });
+
+//Category Edit
+  $(".modal-dialog").on('click', '.updateCat', function() {
+    var currenttr = $(this).closest(".editmodal");
+    var cid = currenttr.find(".cid").html();
+    var cat = currenttr.find("input[name=category]").val();
+    // console.log(cat);
+    $.ajax({
+      url: `/category/edit/${cid}/`,
+      method: "GET",
+      data:{cid:cid, cat:cat},
+      success: function(data){
+        alert('Category Edit from List')
+          window.setTimeout(function(){ } ,2000);
+                          location.reload();
+  
+          // window.location.href = '/admin/';
+      },
+      error:function(){
+          alert('having some error contact to developer');
+      },
+                      
+    });//end ajax
+
+  });
+
+
+//End Category View 
+
+//Start Add to Cart
+$("#productlist").on('click', '.addtocartbtn', function() {
+  var currenttr = $(this).closest(".prolisttr");
+  var cid = currenttr.find("input[name=pid]").val();
+  var pqty = currenttr.find("input[name=pqty]").val();
+  // console.log(cid)
+  $.ajax({
+    url: `/addtocart/`,
+    method: "GET",
+    data:{cid:cid, pqty:pqty},
+    success: function(data){
+      alert('Add to Card')
+        window.setTimeout(function(){ } ,2000);
+                        location.reload();
+
+        // window.location.href = '/admin/';
+    },
+    error:function(){
+        alert('having some error contact to developer');
+    },
+                    
+  });//end ajax
+});
+
+//End Add to Cart
+
+
+
+
+//Customer Change
+$("#invcustomername").change(function(){
+  var a1=$(this).val();
+  var drvcurrenttr = $(this).closest(".drvtbltr");
+  // var dvrtblid = drvcurrenttr.find(".dvrtblid").val();
+  $.ajax({
+  url: "/invcustomername/",
+  method: "GET",
+  data:{a1:a1},
+  success: function(data){
+    window.setTimeout(function(){ } ,2000);
+    location.reload();
+
+  },
+  error:function(){
+      alert('Error contact to 09-969255445');
+  },
+                  
+});//end ajax
+
+});//End Hour1
+
+
+// Print Table 
+$("#sbtn").on('click', '.invsavebtn', function() {
+  let i = $("input[name=customername]").val();
+  console.log(i);
+  // printTable();
+  $.ajax({
+    url: `/invoicesave/`,
+    method: "GET",
+    data:{i:i},
+    success: function(data){
+      printTable();
+      window.setTimeout(function(){ } ,2000);
+                        location.reload();
+                        // printTable();
+        // window.location.href = '/admin/';
+    },
+    error:function(){
+        alert('having some error contact to developer');
+    },
+                    
+  });//end ajax
+
+});
+
+
+
+
+function printTable() {
+	var el=document.getElementById("saleview-1");
+	el.setAttribute('border', '1');
+  el.setAttribute('cellpadding', '10');
+  
+	newPrint=window.open("");
+	newPrint.document.write(el.outerHTML);
+	newPrint.print();
+	newPrint.close();
+}
+
+
+
+
+
+
